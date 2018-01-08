39be7b3d865df45eeb0b26aaef9f97cc7bb84707
John <j.c.rowell15@gmail.com>
2018-01-08 10:45:49 -0500

    calling quote and added functional new quote button

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 function getQuote(){
   $.get("https://random-quote-generator.herokuapp.com/api/quotes/random", function(data){
-    console.log(data);
+    let quote = data.quote;
+    let author = data.author;
+    $('#quote').text(quote);
+    $('#author').text(author);
   })
 }
+
+$("#newQuote").click(getQuote);
 
 getQuote();
