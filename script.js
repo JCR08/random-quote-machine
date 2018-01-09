2dee1e4348d5c6f47c03366ee2aec4a54fd7ebd1
John <j.c.rowell15@gmail.com>
2018-01-09 13:29:17 -0500

    retweet button working

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 function getQuote(){
   $.get("https://random-quote-generator.herokuapp.com/api/quotes/random", function(data){
-    let quote = data.quote;
-    let author = data.author;
+    var quote = data.quote;
+    var author = data.author;
+    console.log(author);
+    $("#tweet").attr("href", tweet(quote, author));
     $('#quote').text(quote);
     $('#author').text(author);
   })
+}
+
+function tweet(quote, author){
+  var twitterURL = `https://twitter.com/intent/tweet?text="`;
+  twitterURL += encodeURIComponent(quote) +'" - '+ encodeURIComponent(author);
+  return twitterURL;
 }
 
 $("#newQuote").click(getQuote);
