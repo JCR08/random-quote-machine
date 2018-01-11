78d9287ebbe326e1858adc39c0a82a3f0054e7f1
John <j.c.rowell15@gmail.com>
2018-01-10 20:58:33 -0500

    added color change animation

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+var colors = ['#16a085', '#27ae60', '#2c3e50', '#f39c12', '#e74c3c', '#9b59b6', '#FB6964', '#342224', "#472E32", "#BDBB99", "#77B1A9", "#73A857"];
+
 function getQuote(){
   $.get("https://random-quote-generator.herokuapp.com/api/quotes/random", function(data){
     var quote = data.quote;
@@ -6,6 +8,11 @@ function getQuote(){
     $('#quote').text(quote);
     $('#author').text(`- ${author}`);
   })
+  var color = colors[Math.floor(Math.random() * colors.length)];
+  console.log(color);
+  $("body").animate({backgroundColor: color, color: color}, "slow");
+  $("button").animate({backgroundColor: color}, "slow");
+  $("i").animate({color: color}, "slow");
 }
 
 function tweet(quote, author){
